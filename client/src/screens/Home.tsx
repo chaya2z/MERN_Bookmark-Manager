e3470c782b1086aaac1151cc8440a22b55ae0df1
chaya2z <itunano57@gmail.com>
2021-02-06 13:57:02 +0900

    [ADD] display GET:/api/bookmarks data

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+const getBookmarks = async () => {
+    try {
+        return await axios.get('http://192.168.11.55:8080/api/bookmarks');
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 const Home: React.FC = () => {
+    const [bookmarkData, setBookmarkData] = useState<any>(null);
+
+    useEffect(() => {
+        let unmounted = false;
+
+        (async () => {
+            let bookmarkData = await getBookmarks();
+            bookmarkData = bookmarkData?.data[0].title;
+
+            if (!unmounted) {
+                setBookmarkData(bookmarkData);
+            }
+        })();
+        return () => {
+            unmounted = true;
+        };
+    }, []);
+
     return (
         <div>
             <h1>Home</h1>
             <p>Hello React</p>
+            <div>
+                {bookmarkData ? <p>title is { bookmarkData }</p> : <p>loading ...</p>}
+            </div>
         </div>
     );
 };
